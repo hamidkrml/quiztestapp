b32992e79ae158d3caa570d8d0d9d8a7dfd35516
hamidkrml <karimlihamid4@gmail.com>
2025-02-27 11:58:03 +0300

    Start

---
+++ quizTestApp/quizTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  quizTestAppApp.swift
+//  quizTestApp
+//
+//  Created by hamid on 27.02.25.
+//
+
+import SwiftUI
+
+@main
+struct quizTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
